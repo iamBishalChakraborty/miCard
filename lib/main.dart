55effa31e697d95a9929ae44537ad563fb3313db
Bishal Chakraborty <cbishal6@gmail.com>
2020-11-14 15:19:12 +0530

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,81 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Center(child: Text('miCard')),
-          backgroundColor: Colors.teal[900],
+        // appBar: AppBar(
+        //   title: Center(child: Text('miCard')),
+        //   backgroundColor: Colors.teal[900],
+        // ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 45.0,
+                backgroundColor: Colors.redAccent,
+                backgroundImage: NetworkImage(
+                    'https://iambishalchakraborty.github.io/assets/IMG_1286p.png'),
+              ),
+              Text(
+                'Bishal Chakraborty',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    letterSpacing: 3,
+                    fontSize: 12,
+                    color: Colors.teal[200],
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'NunitoSans-SemiBold'),
+              ),
+              SizedBox(
+                height: 20,
+                width: 130,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 8794104391',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'NunitoSans',
+                        fontSize: 15),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'cbishal6@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'NunitoSans',
+                        fontSize: 15),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
